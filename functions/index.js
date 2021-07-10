91f093443be0459eb8834dc5136b775f5c896ea5
Karan Navani <kknavani@gmail.com>
2021-07-10 22:14:04 +0100

    added reading habit

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
+const moment = require('moment');
 
-exports.helloWorld = functions
+exports.getHabits = functions
   .region('europe-west2')
   .https.onRequest((request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
+    const habitId = '95356A72-B8D4-4F58-B15B-78210B8511C5';
+    const targetDate = getRecentSunday();
+    const url = `https://api.habitify.me/status/${habitId}?target_date=${targetDate}`;
 
-    fetch('https://api.habitify.me/habits', {
+    fetch(url, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -15,5 +18,12 @@ exports.helloWorld = functions
       },
     })
       .then((response) => response.json())
-      .then((data) => response.send(data));
+      .then((data) => response.send(data.data));
   });
+
+const getRecentSunday = () => {
+  const d = moment().startOf('week');
+  d.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+
+  return encodeURIComponent(d.format());
+};
